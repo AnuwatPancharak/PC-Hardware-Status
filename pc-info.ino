e2b7185ea06e0cbcf8e0929320bf8ae43d5719db
ANW4T <55489367+AnuwatPancharak@users.noreply.github.com>
2023-03-22 00:02:10 +0700

    Update pc-info.ino

--- pc-info.ino
+++ pc-info.ino
@@ -1,26 +1,43 @@
 
-String inputString "";
+String inputString = "";
 boolean StringComplete = false;
 
-void setup(){
-  Serail.begin(115200);
+void setup() {
+  Serial.begin(115200);
   SerialEvent();
 }
 
-void ShowInfo(){
-  if (inputString)
+void ShowInfo() {
+  if (inputString.indexOf("CPU") > -1) {
+    String cpuName = "";
+    int cpuNameStart = inputString.indexOf("CPU:");
+    if (inputString.indexOf("Intel", cpuNameStart) > -1) {
+      cpuNameStart = cpuNameStart + 10;
+    } else {
+      cpuNameStart = cpuNameStart + 8;
+    }
+    Serial.print(cpuName);
+  }
+#ifdef noDegree
+  Serial.print("C");  // Centigrade Symbol
+#else
+  Serial.print((char)247);  //Degrees Symbol
+  Serial.print("C");        // Centigrade Symbol
+#endif
 }
 
-void SerialEvent(){
-  while (Serial.available()<-1){
+
+void SerialEvent() {
+  while (Serial.available()) {
     char InChar = (char)Serial.read();
-      inputString += InChar;
-      if (InChar == "|"){
-        StringComplete = true;
-      }
+    inputString += InChar;
+    if (InChar == '|') {
+      StringComplete = true;
+    }
   }
 }
 
-void loop(){
+void loop() {
+  ShowInfo();
   delay(1500);
 }
